e7b3d12d2cf64770fee70002d86af93e596f8dc0
Ruslan <djvorf@gmail.com>
2021-03-08 23:30:56 +0600

    [ADD] retry

---
+++ postgres_to_es/decos.py
@@ -0,0 +1,33 @@
+import logging
+import time
+from functools import wraps
+
+logger = logging.getLogger()
+
+
+def coroutine(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        fn = func(*args, **kwargs)
+        next(fn)
+        return fn
+
+    return inner
+
+
+def retry(exception_to_check: Exception, tries: int = 4, delay: int = 2, backoff: int = 2, logger=logger):
+    def deco_retry(f):
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except exception_to_check as e:
+                    logger.warning(msg=f"{e}, Retrying in {mdelay} seconds...")
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+        return f_retry
+    return deco_retry
--- postgres_to_es/etl.py
+++ postgres_to_es/etl.py
@@ -5,28 +5,19 @@ import logging
 import requests
 import psycopg2 as psycopg2
 
-from functools import wraps
-
 from psycopg2.extras import DictCursor
 from urllib.parse import urljoin
 
-from models.movie import Movie
+from models import Movie
+from postgres_to_es.decos import coroutine, retry
 
 logger = logging.getLogger()
 
 
-def coroutine(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        fn = func(*args, **kwargs)
-        next(fn)
-        return fn
-    return inner
-
-
+@retry(exception_to_check=Exception)
 @coroutine
-def get_updated_data(connection, target):
-    with connection.cursor() as cursor:
+def get_updated_data(dsl: dict, target):
+    with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn, pg_conn.cursor() as cursor:
         while True:
             table_name = (yield)
             cursor.execute(
@@ -58,30 +49,35 @@ def get_updated_data(connection, target):
                 LIMIT 100 
                 """
             )
-            movies = cursor.fetchall()
-            target.send(movies)
+            values = cursor.fetchall()
+            target.send(values)
 
 
 @coroutine
 def transform_data(target):
+    """
+    Приводит данные к модели
+    :param target: generator
+    """
     while True:
-        data = (yield)
-        record = []
-        for item in data:
-            record.append(Movie(
+        values = (yield)
+        records = []
+        for item in values:
+            records.append(Movie(
                 created=item[0].strftime("%m-%d-%Y:%H:%M:%S"), modified=item[1].strftime("%m-%d-%Y:%H:%M:%S"),
                 id=item[2], title=item[3], description=item[4], create_date=item[5].strftime("%m-%d-%Y"),
                 age_qualification=item[6], rating=item[7], file=item[8], category=item[9][0], genres=item[10],
                 actors=item[11], writers=item[12], directors=item[13]).dict())
-        target.send(record)
+        target.send(records)
+
 
 @coroutine
 def load_to_es(index_name: str):
     """
     Отправка запроса в ES и разбор ошибок сохранения данных
     """
-    url = 'http://localhost:9200/'
     while True:
+        url = 'http://localhost:9201/'
         rows = (yield)
         prepared_query = []
         for row in rows:
@@ -97,7 +93,6 @@ def load_to_es(index_name: str):
             data=str_query,
             headers={'Content-Type': 'application/x-ndjson'}
         )
-
         json_response = json.loads(response.content.decode())
         for item in json_response['items']:
             error_message = item['index'].get('error')
@@ -109,10 +104,9 @@ if __name__ == '__main__':
     dsl = {'dbname': os.environ.get('POSTGRES_DB_NAME'), 'user': os.environ.get('POSTGRES_USER'),
            'password': os.environ.get('POSTGRES_PASSWORD'), 'host': os.environ.get('POSTGRES_HOST'),
            'port': os.environ.get('POSTGRES_PORT')}
-    with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-        load_to_es = load_to_es(index_name='movies')
-        transform_data_movies = transform_data(target=load_to_es)
-        movies = get_updated_data(connection=pg_conn, target=transform_data_movies)
-        movies.send('movie_movie')
-        movies.send('movie_person')
-        movies.send('movie_genre')
+    load_to_es = load_to_es(index_name='movies')
+    transform_data_movies = transform_data(target=load_to_es)
+    movies = get_updated_data(dsl=dsl, target=transform_data_movies)
+    movies.send('movie_movie')
+    movies.send('movie_person')
+    movies.send('movie_genre')
